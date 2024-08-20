bc711bb9eddbe9a217673eebd0dc19faf7c3588d
Nithisx <nithishkumarnk182005@gmail.com>
2024-08-20 10:43:11 +0530

    done

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ mongoose.connect("mongodb://127.0.0.1:27017/student")
         console.error("Failed to connect to MongoDB", err);
     });
 
-// Admin Registration Route
 app.post('/admin', (req, res) => {
     const { name, email, password } = req.body;
 
